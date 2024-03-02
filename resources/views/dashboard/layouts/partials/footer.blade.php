9cedf76d46b1ac11ce72b466428c4cce93d7c947
HamzaAfzaal032 <55828706+HamzaAfzaal032@users.noreply.github.com>
2024-03-02 17:31:58 +0500

    Code Updated

--- resources/views/dashboard/layouts/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer d-flex flex-column align-items-center justify-content-between">
-    <p class="text-muted text-center text-md-left">
-        Copyright © {{date("Y")}} <a href="javascript:void(0);" target="_blank">101 Electronics</a>. All rights reserved
-    </p>
-</footer>
